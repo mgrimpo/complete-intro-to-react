66650b3c124db27cb97d21a84848a93d0000ce34
Marc Grimpo <marc.grimpo@posteo.de>
2019-10-27 20:15:23 +0100

    Add color theme for buttons using React Context

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -2,6 +2,7 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
     state = { loading: true };
@@ -42,7 +43,13 @@ class Details extends React.Component {
                     <h2>
                         {animal} – {breed} – {location}
                     </h2>
-                    <button>Adopt {name}</button>
+                    <ThemeContext.Consumer>
+                        {themeHook => (
+                            <button style={{ backgroundColor: themeHook[0] }}>
+                                Adopt {name}
+                            </button>
+                        )}
+                    </ThemeContext.Consumer>
                     <p>{description}</p>
                 </div>
             </div>
